523ee19ae168d4c7067802d4e546f20f92c378b5
rebita <darklune@naver.com>
2017-06-15 11:02:56 +0900

    mongoosDB test

--- app.js
+++ app.js
@@ -2,7 +2,38 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://test:dadmin01@ds127892.mlab.com:27892/rebita");
+
+var db = mongoose.connection;
+db.once("open",function(){
+  console.log("DB connected");
+});
+db.on("error",function(err){
+    console.log("DB ERROR : ",err);
+});
+
+app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname,'public')));
+
+var data={count:0};
+app.get('/',function(req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count',function(req,res){
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num',function(req,res){
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
 
 app.listen(3000,function (){
   console.log('Server On!');
